a685ee4ba760c1785cfde45f3e9a0b22422d35c0
camilajaviera <cjmunozn@hotmail.com>
2024-12-19 13:46:00 -0300

    fix(app): edit necessary libraries.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import necessary libraries
 
-from flask import Flask, render_template, send_file
+from flask import Flask as f, render_template as rt, send_file as sf
 import os
-from kaggle_connect import kaggle_connectgit 
+from kaggle_connect import kaggle_connect  as kc
