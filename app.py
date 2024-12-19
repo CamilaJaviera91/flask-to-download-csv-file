798eceeac3685ea578e4a1139bfa80feb02e9b20
camilajaviera <cjmunozn@hotmail.com>
2024-12-19 13:50:53 -0300

    feat(app): wrapper function to run Kaggle connect using curses and run it.

--- app.py
+++ app.py
@@ -3,3 +3,11 @@
 from flask import Flask as f, render_template as rt, send_file as sf
 import os
 from kaggle_connect import kaggle_connect  as kc
+import curses as cr
+
+# wrapper function to run Kaggle connect using curses.
+def run_kaggle():
+    return cr.wrapper(kc) # kc is passed as a function, not as a pre-executed call.
+
+# run function
+kc = run_kaggle()
